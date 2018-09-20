5ea32a677bf5db0ad0a7c808633a1ab9db144a01
Alejandro Vivas <alejandrojv17@gmail.com>
2018-09-20 12:49:26 -0300

    Start

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
     
 		<p>I made this Vue 'template' to help me build little projects such as static websites. It comes with Bootstrap 3, JQuery, Awesome Fonts and custom .scss scaffolding.</p>
 		<p  class="mb-40">I hope you find it useful.</p>
-		<a href="http://www.Pixelagil.com/" class="btn btn-lg btn-theme round" target="_blank">Visit my website</a> or <a href="#!" class="btn btn-lg btn-theme border round border-dark" target="_blank"><i class="fa fa-github"></i> Go to Github</a>
+		<a href="http://www.Pixelagil.com/" class="btn btn-lg btn-theme round" target="_blank">Visit my website</a> or <a href="https://github.com/AlexSegen/pixelagil-vuecli-boilerplate" class="btn btn-lg btn-theme border round border-dark" target="_blank"><i class="fa fa-github"></i> Go to Github</a>
 		<p class="mb-40 mt-40">Go <router-link to="/elements">here</router-link> to see bootstrap elements styles</p>
 	</div>
 </template>
